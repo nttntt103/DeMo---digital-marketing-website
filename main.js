7e4381aaa598579f1b81b977263eabdecc71ed45
Saru 3k <(nttntt103@gmail.com)>
2022-08-24 09:40:41 +0700

    first commit

---
+++ main.js
@@ -0,0 +1,30 @@
+// const navBar = document.getElementById('nav-bar')
+
+// window.addEventListener('scroll', () => {
+//       if (document.body.scrollTop > 79 || document.documentElement.scrollTop > 79) {
+//         navBar.current.classList.add('nav-shrink')
+//       } else {
+//         navBar.current.classList.remove('nav-shrink')
+//       }
+//     })
+
+const nav = document.querySelector('.top-section nav')
+
+const navWrapper = document.querySelector(".nav-wrapper");
+const menuBtn = document.querySelector(".menu-btn")
+menuBtn.addEventListener('click', () => {
+    navWrapper.classList.toggle('nav-active')
+    nav.classList.toggle('menuToX')
+})
+
+const navLinks = document.querySelectorAll('.top-section ul li a')
+const navBtn = document.querySelector('.top-section button.red-btn')
+navLinks.forEach(link => {
+    link.addEventListener('click', () => {
+        navWrapper.classList.toggle('nav-active')
+    })
+})
+navBtn.addEventListener("click", () => {
+  navWrapper.classList.toggle("nav-active");
+});
+
